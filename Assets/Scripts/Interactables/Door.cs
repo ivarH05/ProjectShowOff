0e6528cc21eafda559e70079c50ecb99a1d88f93
ivarH05 <119761956+ivarH05@users.noreply.github.com>
2025-05-08 11:43:06 +0200

    Mouse based Door movement
    New: DoorMouseStrategy
    - Rotates the door when moving the mouse forwards or backwards
    - Changes based on which side you are facing
    
    Fix: Limited door angle
    Fix: Inverted Door angle
    Fix: Changed StartAngle and MaxAngle to startAngle and maxAngle
    
    Change: Moving Door will lerp
    Change: Max angle and StartAngle have a range
    
    Note:
    Door requires a PlayerController for now, remove when Interact Event is added

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,3 +1,4 @@
+using Player;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,49 +9,84 @@ namespace Interactables
         [Header("Setup")]
         public Transform hinge;
 
+        [Header("Debug setup")]
+        public PlayerController controller;
+
         [Header("Settings")]
-        public float StartAngle;
-        public float MaxAngle = 120;
+        [Range(0, 180)]
+        public float startAngle;
+        [Range(0, 180)] 
+        public float maxAngle = 120;
+
+        private float _angle;
+
         private void OnDrawGizmos()
         {
+            _angle = startAngle;
             DrawDoorGizmos();
         }
 
         private void OnDrawGizmosSelected()
         {
-            if (MaxAngle < 0 || MaxAngle > 180)
+            if (maxAngle < 0 || maxAngle > 180)
                 Gizmos.color = Color.red;
 
+            SetLocalDoorRotation(startAngle);
             DrawDoorGizmos();
         }
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void Start()
         {
-
+            OnInteract(controller);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
-            //hinge.transform.eulerAngles += new Vector3(0, Input.GetAxis("Mouse Y"), 0);
+            const float speed = 5;
+            float nextAngle = Mathf.Lerp(hinge.localEulerAngles.y, _angle, Time.fixedDeltaTime * speed);
+            SetLocalDoorRotation(nextAngle);
+        }
+
+        void OnInteract(PlayerController controller)
+        {
+            controller.SwitchStrategy(new DoorMouseStrategy(this));
+        }
+
+        public void Move(float deltaAngle)
+        {
+            _angle += deltaAngle;
+            if(_angle < 0)
+                _angle = 0;
+            if(_angle > maxAngle)
+                _angle = maxAngle;
+        }
+
+        private void SetLocalDoorRotation(float newAngle)
+        {
+            hinge.localEulerAngles = new Vector3(
+                hinge.localEulerAngles.x,
+                newAngle,
+                hinge.localEulerAngles.z);
         }
 
         private void DrawDoorGizmos()
         {
+            if (startAngle > maxAngle || _angle > maxAngle)
+                Gizmos.color = Color.red;
+
             const float radius = 0.8f;
 
             List<Vector3> points = new List<Vector3>() { Vector3.zero };
 
-            for (float a = MaxAngle; a > 0; a -= 10)
+            for (float a = -maxAngle; a < 0; a += 10)
             {
-
                 float rads = a * Mathf.Deg2Rad;
                 float x = Mathf.Cos(rads) * radius;
                 float z = Mathf.Sin(rads) * radius;
 
                 points.Add(new Vector3(x, 0f, z));
             }
+
             points.Add(new Vector3(radius, 0f, 0));
 
             Gizmos.matrix = transform.localToWorldMatrix;
---
+++ Assets/Scripts/Interactables/DoorMouseStrategy.cs
@@ -0,0 +1,29 @@
+using Player;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class DoorMouseStrategy : MouseStrategy
+    {
+        private Door door;
+        public DoorMouseStrategy(Door door)
+        {
+            this.door = door;
+        }
+        
+        public override void StartStrategy(PlayerController controller) { }
+        public override void OnLook(PlayerController controller, Vector2 lookDelta)
+        {
+            float DoorMoveSpeed = 0.2f;
+
+            if (Vector3.Dot(door.transform.forward, controller.CameraTransform.forward) > 0)
+                DoorMoveSpeed = -DoorMoveSpeed;
+
+
+            door.Move(lookDelta.y * DoorMoveSpeed);
+        }
+        public override void OnAttack(PlayerController controller) { }
+        public override void OnAttackSecondary(PlayerController controller) { }
+        public override void StopStrategy(PlayerController controller) { }
+    }
+}
