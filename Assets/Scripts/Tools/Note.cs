534df386745e5ad04123faaf015abd2c89495d7f
putrid slurry <547366@student.saxion.nl>
2025-05-15 11:08:21 +0200

    Add note, add temp player sounds
    no functionality yet.

---
+++ Assets/Scripts/Tools/Note.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Tools
+{
+    class Note : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public string Text;
+
+        [CustomEditor(typeof(Note))]
+        public class NoteEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                var t = target as Note;
+                EditorGUI.BeginChangeCheck();
+                var newtxt = EditorGUILayout.TextArea(t.Text);
+                if(EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(t, "Change note");
+                    t.Text = newtxt;
+                    EditorUtility.SetDirty(t);
+                }
+            }
+        }
+#else
+    void Start()
+    {
+        Destroy(this);
+    }
+#endif
+    }
+}
