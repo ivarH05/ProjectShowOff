cb029d932ee303bca78d525d451ec895a7ccd81e
putrid slurry <547366@student.saxion.nl>
2025-05-22 13:50:59 +0200

    Multi-editing... sort of
    Rotating and scaling is pretty janky so ill fix that later

--- Assets/Scripts/CryptBuilder/BuilderEditor.cs
+++ Assets/Scripts/CryptBuilder/BuilderEditor.cs
@@ -1,20 +1,30 @@
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.PackageManager;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace CryptBuilder
 {
     public partial class Builder
     {
-        [SerializeField] bool _holdingRectangle;
         [SerializeField] EditMode _editMode;
-        [SerializeField] RotatedRectangle _heldRectangle;
+        [SerializeField] List<RotatedRectangle> _heldRectangles = new();
 
         [CustomEditor(typeof(Builder))]
         class BuilderEditor : Editor
         {
+            private void OnDisable()
+            {
+                var b = (Builder)target;
+                if(b._heldRectangles.Count > 0)
+                {
+                    Undo.RecordObject(b, "Deselect rectangle");
+                    b._editMode = EditMode.DontEdit;
+                    foreach(RotatedRectangle rect in b._heldRectangles)
+                        b.RectangleTree.AddRectangle(rect);
+                    EditorUtility.SetDirty(b);
+                }
+            }
             public override void OnInspectorGUI()
             {
                 base.OnInspectorGUI();
@@ -36,6 +46,10 @@ namespace CryptBuilder
                     b.RectangleTree.AddRectangle(rect);
                     SceneView.RepaintAll();
                     EditorUtility.SetDirty(b);
+                }
+                if(GUILayout.Button("Add new rect"))
+                {
+                    b._editMode = EditMode.AddNew;
                 }
             }
             void OnSceneGUI()
@@ -61,54 +75,74 @@ namespace CryptBuilder
                 }
             }
 
+            bool GetRectAtMouse(Builder b, out int rect, out int node)
+            {
+                rect = default;
+                node = default;
+
+                if (!TryTracePlaneFromMouse(out var mousePos)) return false;
+
+                return b.RectangleTree.Nodes[1].TryGetRectangleAtPoint(mousePos, b.RectangleTree, out rect, out node);
+            }
+
             void DontEdit(Builder b)
             {
-                if (TryTracePlaneFromMouse(out var mousePos))
+                if(GetRectAtMouse(b, out int rect, out int node))
                 {
-                    if (b.RectangleTree.Nodes[1].TryGetRectangleAtPoint(mousePos, b.RectangleTree, out int rect, out int node))
+                    Handles.color = Color.yellow;
+                    var hovered = b.RectangleTree.Nodes[node].Rectangles[rect];
+                    DrawRectangle(hovered);
+                    if (Event.current.type == EventType.MouseDown && Event.current.button == (int)MouseButton.Left)
                     {
-                        Handles.color = Color.yellow;
-                        var hovered = b.RectangleTree.Nodes[node].Rectangles[rect];
-                        DrawRectangle(hovered);
-                        if (Event.current.type == EventType.MouseDown && Event.current.button == (int)MouseButton.Left)
-                        {
-                            Undo.RecordObject(b, "Select rectangle");
-                            Event.current.Use();
-                            b._heldRectangle = hovered;
-                            b._holdingRectangle = true;
-                            b._editMode = EditMode.EditHeld;
-                            b.RectangleTree.Nodes[node].RemoveRectangle(rect, b.RectangleTree);
-                            EditorUtility.SetDirty(b);
-                        }
+                        Undo.RecordObject(b, "Select rectangle");
+                        Event.current.Use();
+                        b._heldRectangles.Add(hovered);
+                        b._editMode = EditMode.EditHeld;
+                        b.RectangleTree.Nodes[node].RemoveRectangle(rect, b.RectangleTree);
+                        EditorUtility.SetDirty(b);
                     }
-                    if (Event.current.type == EventType.MouseMove)
-                        SceneView.RepaintAll();
                 }
+                if (Event.current.type == EventType.MouseMove)
+                    SceneView.RepaintAll();
             }
 
             void EditHeld(Builder b)
             {
-                if (!b._holdingRectangle)
+                if (b._heldRectangles.Count < 1)
                 {
                     b._editMode = EditMode.DontEdit;
                     return;
                 }
 
-                Handles.color = b._heldRectangle.IsValid ? Color.white : Color.red;
-                DrawRectangle(b._heldRectangle);
-
-                EditorGUI.BeginChangeCheck();
-                Vector3 pos = b._heldRectangle.CenterPosition.To3D();
-                Vector3 scale = b._heldRectangle.HalfSize.To3D();
-                Quaternion rotation = Quaternion.AngleAxis(b._heldRectangle.Rotation, Vector3.up);
-                Handles.TransformHandle(ref pos, ref rotation, ref scale);
-                if (EditorGUI.EndChangeCheck())
+                foreach (var rect in b._heldRectangles)
                 {
-                    Undo.RecordObject(b, "Transform held rectangle");
-                    b._heldRectangle.CenterPosition = pos.To2D();
-                    b._heldRectangle.HalfSize = scale.To2D();
-                    b._heldRectangle.Rotation = rotation.eulerAngles.y;
-                    EditorUtility.SetDirty(b);
+                    Handles.color = rect.IsValid ? Color.white : Color.red;
+                    DrawRectangle(rect);
+                }
+
+                if(!Event.current.shift)
+                {
+                    EditorGUI.BeginChangeCheck();
+                    Vector3 pos = b._heldRectangles[0].CenterPosition.To3D();
+                    Vector3 scale = b._heldRectangles[0].HalfSize.To3D();
+                    Quaternion rotation = Quaternion.AngleAxis(b._heldRectangles[0].Rotation, Vector3.up);
+                    Handles.TransformHandle(ref pos, ref rotation, ref scale);
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        Undo.RecordObject(b, "Transform held rectangle(s)");
+                        Vector2 posDif = pos.To2D() - b._heldRectangles[0].CenterPosition;
+                        Vector2 scaleDif = scale.To2D() / b._heldRectangles[0].HalfSize;
+                        float rotationDif = rotation.eulerAngles.y - b._heldRectangles[0].Rotation;
+                        for(int i = 0; i<b._heldRectangles.Count; i++)
+                        {
+                            var rect = b._heldRectangles[i];
+                            rect.CenterPosition += posDif;
+                            rect.HalfSize *= scaleDif;
+                            rect.Rotation += rotationDif;
+                            b._heldRectangles[i] = rect;
+                        }
+                        EditorUtility.SetDirty(b);
+                    }
                 }
 
                 if(Event.current.type == EventType.KeyDown)
@@ -118,7 +152,7 @@ namespace CryptBuilder
                         case KeyCode.Delete:
                         case KeyCode.Backspace:
                             Undo.RecordObject(b, "Delete held rectangle");
-                            b._holdingRectangle = false;
+                            b._heldRectangles.Clear();
                             b._editMode = EditMode.DontEdit;
                             Event.current.Use();
                             EditorUtility.SetDirty(b);
@@ -128,19 +162,56 @@ namespace CryptBuilder
                             if (Event.current.control)
                             {
                                 Undo.RecordObject(b, "Duplicate held rectangle");
-                                b.RectangleTree.AddRectangle(b._heldRectangle);
+                                foreach(var rect in b._heldRectangles)
+                                    b.RectangleTree.AddRectangle(rect);
                                 EditorUtility.SetDirty(b);
                             }
                             return;
                     }
                 }
 
+                if(Event.current.shift && GetRectAtMouse(b, out int rectIndex, out int node))
+                {
+                    Handles.color = Color.yellow;
+                    var hovered = b.RectangleTree.Nodes[node].Rectangles[rectIndex];
+                    DrawRectangle(hovered);
+                }
                 if(Event.current.type == EventType.MouseDown && Event.current.button == (int)MouseButton.Left)
                 {
-                    Undo.RecordObject(b, "Deselect rectangle");
-                    b._holdingRectangle = false;
+                    if(Event.current.shift)
+                    {
+                        if (!TryTracePlaneFromMouse(out var mousePos))
+                            return;
+                        if(GetRectAtMouse(b, out rectIndex, out node))
+                        {
+                            Undo.RecordObject(b, "Add rectangle to selection");
+                            Event.current.Use();
+                            b._heldRectangles.Add(b.RectangleTree.Nodes[node].Rectangles[rectIndex]);
+                            b.RectangleTree.Nodes[node].RemoveRectangle(rectIndex, b.RectangleTree);
+                            EditorUtility.SetDirty(b);
+                            return;
+                        }
+                        for(int i = 0; i< b._heldRectangles.Count; i++)
+                        {
+                            var rect = b._heldRectangles[i];
+                            if (rect.ContainsPoint(mousePos))
+                            {
+                                Undo.RecordObject(b, "Deselect rectangle");
+                                b._heldRectangles.RemoveAt(i);
+                                if(b._heldRectangles.Count == 0)
+                                    b._editMode = EditMode.DontEdit;
+                                b.RectangleTree.AddRectangle(rect);
+                                Event.current.Use();
+                                EditorUtility.SetDirty(b);
+                                return;
+                            }
+                        }
+                    }
+                    Undo.RecordObject(b, "Deselect rectangle(s)");
                     b._editMode = EditMode.DontEdit;
-                    b.RectangleTree.AddRectangle(b._heldRectangle);
+                    foreach(var rect in b._heldRectangles)
+                        b.RectangleTree.AddRectangle(rect);
+                    b._heldRectangles.Clear();
                     Event.current.Use();
                     EditorUtility.SetDirty(b);
                 }
