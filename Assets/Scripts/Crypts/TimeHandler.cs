1b841186c93e5b956907043979a63be381b67c09
putrid slurry <547366@student.saxion.nl>
2025-06-19 14:15:53 +0200

    Finish TimeHandler off
    Waowe!

--- Assets/Scripts/Crypts/TimeHandler.cs
+++ Assets/Scripts/Crypts/TimeHandler.cs
@@ -1,18 +1,63 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Crypts
 {
     public class TimeHandler : MonoBehaviour
     {
-        [SerializeField] Transform _timerTransform;
-        [SerializeField] float _maxSecondsInCrypt = 100;
-        public float Progress01 => Time.timeSinceLevelLoad / _maxSecondsInCrypt;
+        [SerializeField] float _maxSecondsInCrypt = 15*60;
+        [SerializeField] List<TimedEvent> _events;
+        [SerializeField] Clockhand[] _clockHands;
+
+        private void Awake()
+        {
+            if (_events.Count > 0)
+                _maxSecondsInCrypt = 0;
+         
+            foreach(var e in _events)
+                _maxSecondsInCrypt = Mathf.Max( _maxSecondsInCrypt, e.TimeOfEventSeconds);
+        }
 
         private void Update()
         {
-            if (_timerTransform == null) return;
+            for(int i = 0; i < _events.Count; i++)
+            {
+                if (_events[i].TimeOfEventSeconds > Time.timeSinceLevelLoad) 
+                    continue;
 
-            _timerTransform.rotation = Quaternion.AngleAxis(Progress01*-360, Vector3.forward);
+                _events[i].Event?.Invoke();
+                _events.RemoveAt(i);
+                i--;
+            }
+
+            foreach(var h in _clockHands)
+            {
+                if (h.ClockHandTransform == null) 
+                    return;
+                
+                float t = Time.timeSinceLevelLoad / h.PeriodSeconds;
+                t *= -360;
+                if (float.IsNaN(t)) 
+                    return;
+                
+                h.ClockHandTransform.rotation = Quaternion.AngleAxis(t, Vector3.forward);
+            }
+        }
+
+        [Serializable]
+        struct Clockhand
+        {
+            public float PeriodSeconds;
+            public Transform ClockHandTransform;
+        }
+
+        [Serializable]
+        struct TimedEvent
+        {
+            public float TimeOfEventSeconds;
+            public UnityEvent Event;
         }
     }
 }
