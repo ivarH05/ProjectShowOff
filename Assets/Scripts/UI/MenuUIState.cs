d9187dc420f5155cf98358866040e73683c45cb5
Luna Lua <thuync46@gmail.com>
2025-07-04 13:50:45 +0200

    Pause menu prefab and escape event

--- Assets/Scripts/UI/MenuUIState.cs
+++ Assets/Scripts/UI/MenuUIState.cs
@@ -1,6 +1,18 @@
+using DialogueSystem;
 using UnityEngine;
 
 namespace UI
 {
-    public class MenuUIState : UIState { }
+    public class MenuUIState : UIState
+    {
+        public void Resume()
+        {
+            UIManager.Close();
+        }
+
+        public void ReturntoTitle()
+        {
+            DialogueManager.Clear();
+        }
+    }
 }
